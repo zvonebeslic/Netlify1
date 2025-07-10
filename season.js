fa2af77502329050738edc01247d0f2459520a2d
zvonebeslic <besliczvonimir801@gmail.com>
2025-07-10 20:10:54 +0200

    Update season.js

--- season.js
+++ season.js
@@ -83,7 +83,7 @@ function initializeSeasonToggle() {
   const iconsWrapper = document.getElementById('season-icons');
   const strike = document.getElementById('season-strike');
   
-  document.querySelectorAll('.season-icon').forEach(icon => {
+  document.getElementsById('.season-icon').forEach(icon => {
     icon.addEventListener('click', () => {
       const selected = icon.dataset.season;
 
